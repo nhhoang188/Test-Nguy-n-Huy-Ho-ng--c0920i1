62695052158f15036340abdf01386d4e3a78fefa
nhhoang188 <72209266+nhhoang188@users.noreply.github.com>
2020-10-29 11:38:02 +0700

    done

--- findElement.js
+++ findElement.js
@@ -1,7 +1,10 @@
 function findElement(num, arrNumber) {
+    let result="Giá Trị " + num + " Không có Trong Mảng";
     for (let i=0; i< arrNumber.length; i++) {
         if ( num=== arrNumber[i]){
             document.getElementById('result').innerHTML="Giá Trị " + num + " Nằm Trong Mảng Có Vị Trí Thứ " + i;
+            break;
         }
+        document.getElementById('result').innerHTML=result;
     }
 }
